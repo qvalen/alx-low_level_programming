723e7ce90b93ecf0c571a6d9dcc812ef6ea4d92d
qvalen <chayomzy@gmail.com>
2022-11-20 01:56:00 -0800

    sally

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,75 +1,27 @@
-#include <stdlib.h>
-#include "dog.h"
-
-char *_strdup(char *str);
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * new_dog - create a new dog
- *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of owner of the dog
- *
- * Return: pointer to the new dog instance
- */
-dog_t *new_dog(char *name, float age, char *owner)
+ *  * struct dog - A new type describing a dog.
+ *   * @name: The name of the dog.
+ *    * @age: The age of the dog.
+ *     * @owner: The owner of the dog.
+ *      */
+struct dog
 {
-	dog_t *dg;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	dg = malloc(sizeof(dog_t));
-	if (dg == NULL)
-		return (NULL);
-	dg->name = _strdup(name);
-	if (dg->name == NULL)
-	{
-		free(dg);
-		return (NULL);
-	}
-	dg->age = age;
-	dg->owner = _strdup(owner);
-	if (dg->owner == NULL)
-	{
-		free(dg->name);
-		free(dg);
-		return (NULL);
-	}
-
-	return (dg);
-}
-
+		char *name;
+			float age;
+				char *owner;
+};
 
 /**
- * _strdup - duplicate a string
- *
- * @str: the string to copy
- *
- * Return: pointer to the newly allocated space in memory,
- *	   NULL if @str is null, or insufficient memory available
- */
-char *_strdup(char *str)
-{
-	char *newstr;
-	int i, len;
+ *  * dog_t - Typedef for struct dog
+ *   */
+typedef struct dog dog_t;
 
-	if (str == NULL)
-		return (NULL);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-	len = 0;
-	while (*(str + len++))
-		;
-	newstr = malloc(len * sizeof(char));
-	if (newstr == NULL)
-		return (NULL);
-
-	i = 0;
-	while (*(str + i))
-	{
-		*(newstr + i) = *(str + i);
-		++i;
-	}
-	*(newstr + i) = '\0';
-	return (newstr);
-}
+#endif
