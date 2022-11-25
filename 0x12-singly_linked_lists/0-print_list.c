dcf8d55ed019c67b87fa1ccd77b19753e666da4f
qvalen <chayomzy@gmail.com>
2022-11-24 18:22:26 -0800

    soar

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,46 @@
-#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+long _strlen(char *str);
+/**
+ * print_list - prints a linked list
+ *
+ * @head: pointer to the head node
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *head)
+{
+	size_t length;
+
+	length = 0;
+	while (head != NULL)
+	{
+		length++;
+		if (head->str)
+			printf("[%lu] %s\n", _strlen(head->str), head->str);
+		else
+			printf("[0] (nil)\n");
+		head = head->next;
+	}
+	return (length);
+}
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * _strlen - compute the length of a string
  *
- * Return: The number of nodes in h.
+ * @str: the string to process
+ *
+ * Return: length of the string
  */
-
-size_t print_list(const list_t *h)
+long _strlen(char *str)
 {
-	size_t nodes = 0;
+	long length;
 
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
+	length = 0;
+	while (*(str + length))
+		length++;
 
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+	return (length);
 }
