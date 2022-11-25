9a9bd4d90e28ea66346f37fb38d4f197317f4387
qvalen <chayomzy@gmail.com>
2022-11-24 18:04:21 -0800

    sleepy

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,29 +1,22 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
- *  * File: lists.h
- *   * Auth: Brennan D Baraban
- *    * Desc: Header file containing prototypes and definitions for all functions
- *     *       and types written in the 0x11-singly_linked_lists directory.
- *      */
-
-#include <stdlib.h>
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 /**
  *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        *              for Holberton project
- *         */
+ *   *
+ *    * @str: malloc'ed string
+ *     * @len: length of the string
+ *      * @next: pointer to next node
+ *       *
+ *        * Description: singly linked list node structre
+ *         *
+ *          */
 typedef struct list_s
 {
 		char *str;
 			unsigned int len;
 				struct list_s *next;
+
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -31,5 +24,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print_first(void);
 
-#endif /* LISTS_H */
+#endif /* _LISTS_H_ */
